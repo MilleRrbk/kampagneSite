1063cb7ccb810d21c9042bfe2b23a47ed693995c
MilleRrbk <mille.rorbak@hotmail.com>
2024-06-10 22:10:55 +0200

    setup changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainSand: '#CFB999',
+        mainSalmon: '#B87A64',
+        mainRed: '#931A1D',
+        mainBlack: '#0F0B0A',
+      },
+    },
   },
   plugins: [],
 }
