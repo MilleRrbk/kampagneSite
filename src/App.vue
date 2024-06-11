ed99d57ba03a0fd3b65b1b49e7a4e02576d5862e
MilleRrbk <mille.rorbak@hotmail.com>
2024-06-11 15:21:56 +0200

    Footer

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,46 @@
-<script>
-import { RouterLink, RouterView } from 'vue-router'
-
-export default {
-  components: {
-    RouterView,
-    RouterLink
-  },
-};
-</script>
-
 <template>
+  <div id="app">
+  <header>
+    
+  </header>
 
+  <main>
+    
+  </main>
+
+<Footer> </Footer>
+</div>
 </template>
 
-<style scoped>
+  <script>
+  import { RouterLink, RouterView } from 'vue-router'
+  import Footer from './components/Footer.vue'
+  
+  export default {
+    components: {
+      RouterView,
+      RouterLink,
+      Footer
+    },
+  };
+  </script>
 
+<style scoped>
+/* Global styles can go here */
+#app {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+
+header {
+  background-color: #5b3a3a;
+  padding: 10px;
+  text-align: center;
+}
+
+main {
+  flex: 1;
+  padding: 20px;
+}
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<!-- src/components/Footer.vue -->
+<template>
+    <footer class="footer bg-mainBlack text-mainSand">
+      <!-- Footer content goes here -->
+      <p>Metropolis CullBerg</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Footer'
+  };
+  </script>
+  
+  <style scoped>
+  .footer {
+    text-align: center;
+    padding: 20px;
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
+  </style>
+  
